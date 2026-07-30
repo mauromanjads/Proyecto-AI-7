391f520f815b804a83b0dbc9dab05d2a036de51d
MauricioManjarres <mauricio.manjarres@polaria.tech>
2026-07-29 21:13:42 -0500

    modo prueba

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ import Prologo from "./ui/Prologo.js";
 import TableroAgentes from "./ui/TableroAgentes.js";
 import PointerGlobal from "./ui/PointerGlobal.js";
 
-const MODO_PRUEBA = false;
+const MODO_PRUEBA = true;
 
 const pantallaPrologo =
     document.getElementById("pantalla-prologo");
